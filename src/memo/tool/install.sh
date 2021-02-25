fc95f11234ba6e7e478106d9d21b88b7dbcb11cd
wangwei <2862379252@qq.com>
2021-02-25 12:19:28 +0800

    feat: tool install shell

---
+++ src/memo/tool/install.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+
+
+
+# echo '安装 Command Line Tools'
+# xcode-select --install
+
+# echo '安装 brew'
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# echo '安装 oh-my-zsh'
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# echo '安装 oh-my-zsh 插件 zsh-syntax-highlighting'
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# echo '安装 oh-my-zsh 插件 zsh-autosuggestions'
+# git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# echo '安装 sdkman'
+# curl -s "https://get.sdkman.io" | bash
+
+# echo '***** brew 命令执行 *****'
+# brew install git gh tree htop git-flow-avh redis node wget 
+
+# echo '安装系统增强软件'
+# brew install --cask iterm2 alfred appcleaner sogouinput motrix \
+# google-chrome mounty
+
+# echo '安装开发/效率软件'
+# brew install --cask jetbrains-toolbox visual-studio-code postman typora \
+# dash sourcetree xmind-zen pdf-expert
+
+# echo '安装 Jdk'
+# brew tap AdoptOpenJDK/openjdk
+# brew install --cask adoptopenjdk8
+
+# echo '***** sdkman 命令执行 *****'
