cb15a306b306b4e0e34d670ae103a921e0c88112
wangwei <2862379252@qq.com>
2021-03-06 21:34:20 +0800

    迁移文档 vuepress ==> docusaurus

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,4 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3',],
-  },
   java: {
     基础: [
       'java/README',
@@ -16,6 +13,30 @@ module.exports = {
       'java/base/函数式接口',
       'java/base/反射',
       'java/base/数据类型',
+    ],
+    集合: [
+      'java/collection/Collection',
+      'java/collection/List',
+      'java/collection/Map',
+    ],
+    高并发: [
+      'java/hc/高并发',
+      'java/hc/多线程',
+      'java/hc/并发编程的艺术',
+      'java/hc/内存模型',
+      'java/hc/原子类',
+      'java/hc/并发容器',
+      'java/hc/并发工具类',
+      'java/hc/线程池',
+      'java/hc/锁'
+    ],
+    'JVM': [
+      'java/jvm/对象创建过程',
+      'java/jvm/对象回收机制',
+      'java/jvm/jvm参数',
+      'java/jvm/jvm内存模型',
+      'java/jvm/内存优化',
+      'java/jvm/垃圾收集器',
     ]
   }
 };
