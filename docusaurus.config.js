604cdd167ac9f2ad4cb0587719f48f52d4f97712
wangwei <2862379252@qq.com>
2021-03-06 17:32:32 +0800

    初始化 docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: '进阶之路',
+  tagline: '',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'wangwei461', // Usually your GitHub org/user name.
+  projectName: 'docs.advanced.way', // Usually your repo name.
+  themeConfig: {
+    hideableSidebar: true,
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    liveCodeBlock: {
+      playgroundPosition: 'bottom',
+    },
+    prism: {
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },
+    navbar: {
+      hideOnScroll: true,
+      title: '进阶之路',
+      logo: {
+        alt: '进阶之路 Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'right',
+        },
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
+          dropdownItemsAfter: [
+            {
+              to: '/versions',
+              label: 'All versions',
+            },
+          ],
+        },
+        { to: 'blog', label: '博客', position: 'right' },
+        {
+          href: 'https://github.com/wangwei461/docs.advanced.way.git',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} 进阶之路, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+          showLastUpdateTime: true,
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          postsPerPage: 3,
+          blogSidebarCount: 'ALL',
+          // blogSidebarTitle: 'All our posts',
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
